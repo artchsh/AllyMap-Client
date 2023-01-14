37512098cbbd621b3c05840012f1f718364dda05
Artyom Chshyogolev <bogshelest@gmail.com>
2023-01-15 03:21:07 +0600

    fast fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,16 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@mui")) return "mui"
-            return "vendor"
-          }
-        }
-      }
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("@mui")) return "mui"
+  //           return "vendor"
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 })
